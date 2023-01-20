f853c5e4b3d91f1abb1e9edbe008edd00b32ade6
Strizh.ko <dron-terrorist@mail.ru>
2023-01-20 14:32:29 +0300

    Выполнено задание 7

--- Module14/07_years/main.py
+++ Module14/07_years/main.py
@@ -1,10 +1,15 @@
-a = int(input("Введите первый год: "))
-b = int(input("Введите второй год: "))
-
-print(f"Годы от {a} до {b} с тремя одинаковыми цифрами:")
-
 def find_num(a, b):
     for i in range (a, b + 1):
-    n = str(i)
-    a = str(a)
-    replace()
+        for n in range(10):
+            i = str(i)
+            n = str(n)
+            selected_number = i.replace(n, "")
+            if len(i) - len(selected_number) >= 3:
+                print(i)
+
+a = int(input("Введите первое число: "))
+b = int(input("Введите второе число: "))
+
+print(f"Числа от {a} до {b} с тремя одинаковыми цифрами:")
+
+find_num(a, b)
