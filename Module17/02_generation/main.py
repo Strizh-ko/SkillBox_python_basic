710cc001f190e41d0f5ea07222919326ef5c460f
Andrey.Strizhko <AStrizh90@gmail.com>
2023-02-06 20:25:11 +0300

    Подправил 2, 9

--- Module17/02_generation/main.py
+++ Module17/02_generation/main.py
@@ -1,9 +1,9 @@
-def listgen(i):
-    if i % 2 == 0:
-        return 1
-    else:
-        return i % 5
+# def listgen(i):
+#     if i % 2 == 0:
+#         return 1
+#     else:
+#         return i % 5
 
 n = int(input("Введите длину списка: "))
-sheet = [listgen(x) for x in range(n)]
+sheet = [1 if i % 2 == 0 else i % 5 for i in range(n)]
 print("Результат:", sheet)
--- Module17/09_list_of_lists_2/main.py
+++ Module17/09_list_of_lists_2/main.py
@@ -1,6 +1,6 @@
 nice_list = [[[1, 2, 3], [4, 5, 6], [7, 8, 9]],
              [[10, 11, 12], [13, 14, 15], [16, 17, 18]]]
 
-fine_list = [nice_list[i][0] + nice_list[i][1] + nice_list[i][2] for i in range(2)]
-perfect_list = fine_list[0] + fine_list[1]
+
+perfect_list = [x for fine_list in nice_list for each_list in fine_list for x in each_list]
 print(perfect_list)
