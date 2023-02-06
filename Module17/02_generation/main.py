c302aa0e7bea135c2c1780c344295f2e9bd165ef
Andrey.Strizhko <AStrizh90@gmail.com>
2023-02-06 20:25:11 +0300

    Подправил 2, 9

--- Module17/02_generation/main.py
+++ Module17/02_generation/main.py
@@ -1,9 +1,5 @@
-# def listgen(i):
-#     if i % 2 == 0:
-#         return 1
-#     else:
-#         return i % 5
+
 
 n = int(input("Введите длину списка: "))
-sheet = [1 if i % 2 == 0 else i % 5 for i in range(n)]
+sheet = [listgen(x) for x in range(n)]
 print("Результат:", sheet)
