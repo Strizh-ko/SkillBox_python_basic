fc6fe8470da365c49c151a4c3170eeecf32bd4d5
Andrey.Strizhko <AStrizh90@gmail.com>
2023-02-11 19:31:42 +0300

    Переписал 10е

--- Module18/10_the_truth/main.py
+++ Module18/10_the_truth/main.py
@@ -1,22 +1,20 @@
-def cypher(char, k):
-    alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-
-    if char in alphabet:
-        if alphabet.index(char) + k > len(alphabet) - 1:
-            n_char = alphabet[alphabet.index(char) + k - len(alphabet)]
-        else:
-            n_char = alphabet[alphabet.index(char) + k]
-    else:
-        n_char = char
-    return n_char
-
+alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 text = 'vujgvmCfb tj ufscfu ouib z/vhm jdjuFyqm jt fscfuu uibo jdju/jnqm fTjnqm tj scfuuf ibou fy/dpnqm yDpnqmf jt cfuufs boui dbufe/dpnqmj uGmb tj fuufsc ouib oftufe/ bstfTq jt uufscf uibo otf/ef uzSfbebcjmj vout/dp djbmTqf dbtft (ubsfo djbmtqf hifopv up csfbl ifu t/svmf ipvhiBmu zqsbdujdbmju fbutc uz/qvsj Fsspst tipvme wfsof qbtt foumz/tjm omfttV mjdjumzfyq odfe/tjmf Jo fui dfgb pg hvjuz-bncj gvtfsf fui ubujpoufnq up ftt/hv Uifsf vmetip fc pof.. boe sbcmzqsfgf zpom pof pvt..pcwj xbz pu pe ju/ Bmuipvhi uibu bzx bzn puo cf wjpvtpc bu jstug ttvomf sfzpv( i/Evud xOp tj scfuuf ibou /ofwfs uipvhiBm fsofw jt fopgu cfuufs boui iu++sjh x/op gJ ifu nfoubujpojnqmf tj eibs pu mbjo-fyq tju( b bec /jefb Jg fui foubujpojnqmfn jt fbtz up bjo-fyqm ju znb cf b hppe jefb/ bnftqbdftO bsf pof ipoljoh sfbuh efbj .. fu(tm pe psfn gp tf"uip'
-text = text.upper()
+text = list(text.upper())
+number = [x for x in range(26)]
 
 
-for k in range(26):
-    word_cypher = ""
-    for char in text:
-        word_cypher += cypher(char, k)
 
-    print(f'\n k = {k}', word_cypher)
+new_message_index = [alphabet.index(char) if char in alphabet else char for char in text]
+new_message = ""
+
+
+for k in range(0, 26):
+    new_message = ""
+    for char_i in new_message_index:
+        if char_i in range(26):
+            new_message += alphabet[((char_i + k) + 26) % 26]
+        else:
+            new_message += char_i
+
+    print('\nk =', k, '\n', new_message)
