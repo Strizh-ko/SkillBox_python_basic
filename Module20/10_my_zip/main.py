5691ed39ed698c5f560f916b9ef9e1c4f5f05f25
Andrey.Strizhko <AStrizh90@gmail.com>
2023-02-16 22:47:07 +0300

    Переделал 10е

--- Module20/10_my_zip/main.py
+++ Module20/10_my_zip/main.py
@@ -1,12 +1,12 @@
-def func(line, tpl):
-    pair = zip(line, tpl)
-    return pair
-
+def minimum(line, tpl):
+    return min(len(line), len(tpl))
 
 line = 'abcd'
 tpl = (10, 20, 30, 40)
 
-print(func(line, tpl))
-for i in func(line, tpl):
+pair = ((line[i], tpl[i]) for i in range(minimum(line, tpl)))
+
+print(pair)
+for i in pair:
     print(i)
 
