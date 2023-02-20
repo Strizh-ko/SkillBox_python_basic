af980fed5fd9578127ec1d66581910c6fedc4911
Andrey.Strizhko <AStrizh90@gmail.com>
2023-02-20 19:09:55 +0300

    Доделал 9е задание

--- Module20/09_competition_protocol/main.py
+++ Module20/09_competition_protocol/main.py
@@ -8,15 +8,14 @@ for i in range(n):
     play = input(f'{i + 1}-я запись: ').split()
     if play[1] in registr:
         if int(registr[play[1]][0]) < int(play[0]):
-            registr[play[1]] = [int(play[0]), i + 1]
+            registr[play[1]] = [int(play[0]), -(i + 1)]
     else:
-        registr[play[1]] = [play[0], i + 1]
+        registr[play[1]] = [int(play[0]), -(i + 1)]
 
 score_list = list(registr.items())
 
-print('\nИтоги соревнований:')
 score_list.sort(key=score_key, reverse=True)
-
+print('\nИтоги соревнований:')
 for i in range(3):
     print(f'{i + 1}е место. {score_list[i][0]} ({score_list[i][1][0]})')
 
