67b1e93fb4598eb545d692268ca13d80d998a01e
Andrey.Strizhko <AStrizh90@gmail.com>
2023-02-22 22:39:03 +0300

    Чуть поправил 8е

--- Module21/08_list_of_lists_2/main.py
+++ Module21/08_list_of_lists_2/main.py
@@ -1,8 +1,8 @@
 nice_list = [1, 2, [3, 4], [[5, 6, 7], [8, 9, 10]],
              [[11, 12, 13], [14, 15], [16, 17, 18]]]
 
-def lists(groups, names=None):
-    names = names or []
+def lists(groups):
+    names = []
     flag = True
     for group in groups:
         if isinstance(group, list):
