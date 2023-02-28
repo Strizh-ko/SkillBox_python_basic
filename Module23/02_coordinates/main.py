eafedfff568d27a1869577d86bfedb16a5659f01
Andrey.Strizhko <AStrizh90@gmail.com>
2023-02-28 23:26:16 +0300

    Поправил 2, 4

--- Module23/02_coordinates/main.py
+++ Module23/02_coordinates/main.py
@@ -1,37 +1,37 @@
 import random
 
+
 def f_p(x, y):
-    try:
-        x += random.randint(0, 10)
-        y += random.randint(0, 5)
+    x += random.randint(0, 10)
+    y += random.randint(0, 5)
+    if y == 0:
+        raise ZeroDivisionError('Ошибка в функции №1')
+    else:
         return round(x / y, 2)
-    except ZeroDivisionError:
-        print("Что-то пошло не так с первой функцией")
+
 
 def f_m(x, y):
-    try:
-        x -= random.randint(0, 10)
-        y -= random.randint(0, 5)
+    x -= random.randint(0, 10)
+    y -= random.randint(0, 5)
+    if x == 0:
+        raise ZeroDivisionError('Ошибка в функции №2')
+    else:
         return round(y / x, 2)
-    except ZeroDivisionError:
-        print("Что-то пошло не так со второй функцией")
 
 
+with \
+        open("coordinates.txt", 'r', encoding='utf-8') as file, \
+        open('result.txt', 'w', encoding='utf-8') as file_2:
 
-with open("coordinates.txt", 'r', encoding='utf-8') as file:
-    with open('result.txt', 'w', encoding='utf-8') as file_2:
-
-        for line in file:
-            nums_list = line.split()
+    for line in file:
+        nums_list = line.split()
+        try:
             res1 = f_p(int(nums_list[0]), int(nums_list[1]))
             res2 = f_m(int(nums_list[0]), int(nums_list[1]))
-
+        except ZeroDivisionError as error:
+            print(error)
+        else:
             number = random.randint(0, 100)
-            try:
-                my_list = sorted([res1, res2, number])
-                my_list = map(str, my_list)
-                file_2.write('  '.join(my_list) + '\n')
-            except Exception:
-                print("Что-то пошло не так при записи")
-
-
+            my_list = sorted([res1, res2, number])
+            my_list = map(str, my_list)
+            file_2.write('  '.join(my_list) + '\n')
--- Module23/04_registration/main.py
+++ Module23/04_registration/main.py
@@ -1,30 +1,27 @@
+def line_check(data_lst):
+    if len(data_lst) != 3:
+        raise IndexError('НЕ присутствуют все три поля')
+    elif not data_lst[0].isalpha():
+        raise NameError('Поле «Имя» содержит НЕ только буквы')
+    elif "@" and '.' not in data_lst[1]:
+        raise SyntaxError('Поле «Имейл» НЕ содержит @ и . (точку)')
+    elif int(data_lst[2]) <= 10 or int(data_lst[2]) >= 99 or not data_lst[2].isdigit():
+        raise ValueError('Поле «Возраст» НЕ является числом от 10 до 99')
+    return
+
+
 def reg_sort(data_file):
-    with open(data_file, 'r', encoding='utf-8') as registrations:
+    with open(data_file, 'r', encoding='utf-8') as registrations,\
+        open('registrations_bad.log', 'a', encoding='utf-8') as reg_bad,\
+        open('registrations_good.log', 'a', encoding='utf-8') as reg_good:
+
         for line in registrations:
             data_lst = line.split()
-
-            with open('registrations_bad.log', 'a', encoding='utf-8') as reg_bad:
-                try:
-                    if len(data_lst) != 3:
-                        raise IndexError
-                    if not data_lst[0].isalpha():
-                        raise NameError
-                    if "@" and '.' not in data_lst[1]:
-                        raise SyntaxError
-                    if int(data_lst[2]) <= 10 or int(data_lst[2]) >= 99 or not data_lst[2].isdigit():
-                        raise ValueError
-
-                except IndexError:
-                        reg_bad.write(' '.join(data_lst) + '    -    НЕ присутствуют все три поля\n')
-                except NameError:
-                        reg_bad.write(' '.join(data_lst) + '    -    Поле «Имя» содержит НЕ только буквы\n')
-                except SyntaxError:
-                        reg_bad.write(' '.join(data_lst) + '    -    Поле «Имейл» НЕ содержит @ и . (точку)\n')
-                except ValueError:
-                        reg_bad.write(' '.join(data_lst) + '    -    Поле «Возраст» НЕ является числом от 10 до 99\n')
-
-                else:
-                    with open('registrations_good.log', 'a', encoding='utf-8') as reg_good:
-                        reg_good.write(' '.join(data_lst) + '\n')
+            try:
+                line_check(data_lst)
+            except (IndexError, NameError, SyntaxError, ValueError) as error:
+                reg_bad.write(' '.join(data_lst) + f'    -    {error}\n' )
+            else:
+                reg_good.write(' '.join(data_lst) + '\n')
 
 reg_sort('registrations.txt')
