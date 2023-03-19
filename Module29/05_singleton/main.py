257a3662f5ac3a3b5551e4b2a23035d9801bc523
Andrey.Strizhko <AStrizh90@gmail.com>
2023-03-20 00:45:53 +0300

    Revert "Добавил 4е"
    This reverts commit ace1bd4b

--- Module29/05_singleton/main.py
+++ Module29/05_singleton/main.py
@@ -1,1 +1,23 @@
-# TODO здесь писать код
+def singleton(cls):
+
+    def wrapper(*args, **kwargs):
+        if not wrapper.obj:
+            wrapper.obj = cls(*args, **kwargs)
+        return wrapper.obj
+
+    wrapper.obj = None
+    return wrapper
+
+
+@singleton
+class Example:
+    pass
+
+
+my_obj = Example()
+my_another_obj = Example()
+
+print(id(my_obj))
+print(id(my_another_obj))
+
+print(my_obj is my_another_obj)
