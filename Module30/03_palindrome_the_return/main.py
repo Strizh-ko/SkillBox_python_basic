78e2a54ae6c2e12bcc0f24cc0eb6746c237b324e
Andrey.Strizhko <AStrizh90@gmail.com>
2023-03-21 15:42:46 +0300

    ДЗ готово

--- Module30/03_palindrome_the_return/main.py
+++ Module30/03_palindrome_the_return/main.py
@@ -1,1 +1,13 @@
-# TODO здесь писать код
+from collections import Counter
+
+
+def can_be_poly(line: str) -> bool:
+    cnt = Counter(line)
+    extra = list(filter(lambda x: x % 2, cnt.values()))
+
+    return len(extra) <= 1
+
+
+print(can_be_poly('abcba'))
+print(can_be_poly('abbbc'))
+print(can_be_poly('арозаупаланалапуазора'))
